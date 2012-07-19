50f39e53575403029ce4d0391f1ec17f6e9e7bd4
Damian Conway <damian@conway.org>
2012-07-19 23:34:07 +0000

    Import of DCONWAY/Acme-Bleach-1.014 from CPAN.
    gitpan-cpan-distribution: Acme-Bleach
    gitpan-cpan-version:      1.014
    gitpan-cpan-path:         DCONWAY/Acme-Bleach-1.014.tar.gz
    gitpan-cpan-author:       DCONWAY
    gitpan-cpan-maturity:     released

--- lib/Acme/Bleach.pm
+++ lib/Acme/Bleach.pm
@@ -1,5 +1,5 @@
 package Acme::Bleach;
-our $VERSION = '1.13';
+our $VERSION = '1.014';
 my $tie = " \t"x8;
 sub whiten { local $_ = unpack "b*", pop; tr/01/ \t/; s/(.{9})/$1\n/g; $tie.$_ }
 sub brighten { local $_ = pop; s/^$tie|[^ \t]//g; tr/ \t/01/; pack "b*", $_ }
@@ -8,11 +8,12 @@ sub dress { $_[0] =~ /^$tie/ }
 open 0 or print "Can't rebleach '$0'\n" and exit;
 (my $shirt = join "", <0>) =~ s/(.*)^\s*use\s+Acme::Bleach\s*;\n//sm;
 my $coat = $1;
+my $pressed = '#line ' . ("$coat\n" =~ tr/\n/\n/) . ' ' . (caller)[1] . "\n";
 local $SIG{__WARN__} = \&dirty;
 do {eval $coat . brighten $shirt; print STDERR $@ if $@; exit}
-	unless dirty $shirt && not dress $shirt;
+    unless dirty $shirt && not dress $shirt;
 open 0, ">$0" or print "Cannot bleach '$0'\n" and exit;
-print {0} "${coat}use Acme::Bleach;\n", whiten $shirt and exit;
+print {0} "${coat}use Acme::Bleach;\n", whiten $pressed.$shirt and exit;
 __END__
 
 =head1 NAME
@@ -21,9 +22,9 @@ Acme::Bleach - For I<really> clean programs
 
 =head1 SYNOPSIS
 
-	use Acme::Bleach;
+    use Acme::Bleach;
 
-	print "Hello world";
+    print "Hello world";
 
 =head1 DESCRIPTION
 
@@ -32,27 +33,27 @@ removes all the unsightly printable characters from your source file.
 The code continues to work exactly as it did before, but now it
 looks like this:
 
-	use Acme::Bleach;
-	 	 	 	 	 	 	 	 	 	 	     
-	   			  	
-	  			 	  
-		 		  			
-	 		   	 	
-			      	
-	   	   	 
-	    	  	 
-		 	  		  
-	 		 		   
-			 		 			
-		 		     
-	 	  			 	
-			 				 	
-		  	  			
-	   		 		 
-	  	  		  
-		   	  		
-	 			   	 
-		    
+    use Acme::Bleach;
+     	 	 	 	 	 	 	 	 	 	     
+       			  	
+      			 	  
+    	 		  			
+     		   	 	
+    		      	
+       	   	 
+        	  	 
+    	 	  		  
+     		 		   
+    		 		 			
+    	 		     
+     	  			 	
+    		 				 	
+    	  	  			
+       		 		 
+      	  		  
+    	   	  		
+     			   	 
+    	    
 
 =head1 DIAGNOSTICS
 
@@ -82,3 +83,4 @@ Damian Conway (as if you couldn't guess)
  This module is free software. It may be used, redistributed
 and/or modified under the terms of the Perl Artistic License
      (see http://www.perl.com/perl/misc/Artistic.html)
+
